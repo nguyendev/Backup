a402e302e9408752fce8eb72c8b9f042009e28a1
Nguyễn An Hoàng Nguyên <nguyen.nah76@gmail.com>
2024-04-02 12:25:00 +0700

    fix etc

--- BackUpWindowsForm/Model.Designer.cs
+++ BackUpWindowsForm/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\HoangNguyen\source\repos\BackUp\BackUpWindowsForm\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'I:\Pod\tool\Backup\BackUpWindowsForm\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
